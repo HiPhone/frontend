d7373bd7be37babc8461f9d36e79a3df4d7428f5
HiPhone <zhyzyhf@gmail.com>
2018-11-24 21:29:04 +0800

    angular官方示例，学习到服务层面

---
+++ angular/official-project/src/app/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from './hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
+  //注入heroService解耦
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = this.heroes);
+  }
+
+}
